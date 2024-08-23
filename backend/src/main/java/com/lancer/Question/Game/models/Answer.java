a3df1fede3b74e5dfc89541b67c1ee1b9b3fd220
Plamen Georgiev <plamen.georgiev92@gmail.com>
2024-08-24 00:30:09 +0300

    getter and setters for answer class

--- backend/src/main/java/com/lancer/Question/Game/models/Answer.java
+++ backend/src/main/java/com/lancer/Question/Game/models/Answer.java
@@ -2,7 +2,9 @@ package com.lancer.Question.Game.models;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 
+@Entity
 public class Answer {
 
     @Id
@@ -18,16 +20,58 @@ public class Answer {
     @JoinColumn(name="question_id", nullable=false)
     private Question question;
 
-
-
-
-
-
-    public void setCorrect(){
-        this.isCorrect = true;
+    public Integer getId() {
+        return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public String getAnswerText() {
+        return answerText;
+    }
 
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
 
+    public Boolean getCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(Boolean correct) {
+        isCorrect = correct;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", answerText='" + answerText + '\'' +
+                ", isCorrect=" + isCorrect +
+                ", question=" + question +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answer answer = (Answer) o;
+        return Objects.equals(id, answer.id) && Objects.equals(answerText, answer.answerText) && Objects.equals(isCorrect, answer.isCorrect) && Objects.equals(question, answer.question);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, answerText, isCorrect, question);
+    }
 }
